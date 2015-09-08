ef7cdaae4d948082e5b01d3a20b7130255954dcc
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-09-08 16:23:52 -0700

    add rabbit and base worker unit test

---
+++ test/unit/external/rabbitmq.js
@@ -0,0 +1,240 @@
+'use strict';
+
+require('loadenv')();
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var afterEach = lab.afterEach;
+var beforeEach = lab.beforeEach;
+var Code = require('code');
+var expect = Code.expect;
+
+var sinon = require('sinon');
+var hermesClient = require('runnable-hermes');
+var clone = require('101/clone');
+
+var rabbitClient = require('../../../lib/external/rabbitmq.js');
+
+describe('rabbitmq.js unit test', function () {
+  describe('connect', function() {
+    var hermesMock;
+    beforeEach(function(done) {
+      hermesMock = {
+        connect: sinon.stub()
+      };
+      sinon.stub(hermesClient, 'hermesSingletonFactory').returns(hermesMock);
+      done();
+    });
+
+    afterEach(function(done) {
+      hermesClient.hermesSingletonFactory.restore();
+      done();
+    });
+
+    it('should connect', function(done) {
+      hermesMock.connect.yieldsAsync();
+
+      rabbitClient.connect(function (err) {
+        expect(err).to.not.exist();
+        done();
+      });
+    });
+  }); // end connect
+
+  describe('loadWorkers', function() {
+    beforeEach(function(done) {
+      rabbitClient.hermesClient = {
+        subscribe: sinon.stub()
+      };
+      done();
+    });
+
+    it('should subscribe health-check', function(done) {
+      rabbitClient.hermesClient.subscribe.returns();
+
+      rabbitClient.loadWorkers();
+      expect(rabbitClient.hermesClient.subscribe
+        .withArgs('health-check').called).to.be.true();
+      done();
+    });
+
+    it('should subscribe docker-health-check', function(done) {
+      rabbitClient.hermesClient.subscribe.returns();
+
+      rabbitClient.loadWorkers();
+      expect(rabbitClient.hermesClient.subscribe
+        .withArgs('docker-health-check').called).to.be.true();
+      done();
+    });
+  }); // end loadWorkers
+
+  describe('unloadWorkers', function() {
+    beforeEach(function(done) {
+      rabbitClient.hermesClient = {
+        unsubscribe: sinon.stub()
+      };
+      done();
+    });
+
+    it('should unsubscribe health-check', function(done) {
+      rabbitClient.hermesClient.unsubscribe.yieldsAsync();
+
+      rabbitClient.unloadWorkers(function (err) {
+        expect(err).to.not.exist();
+        expect(rabbitClient.hermesClient.unsubscribe
+          .withArgs('health-check').called).to.be.true();
+        done();
+      });
+    });
+
+    it('should unsubscribe docker-health-check', function(done) {
+      rabbitClient.hermesClient.unsubscribe.yieldsAsync();
+
+      rabbitClient.unloadWorkers(function (err) {
+        expect(err).to.not.exist();
+        expect(rabbitClient.hermesClient.unsubscribe
+          .withArgs('docker-health-check').called).to.be.true();
+        done();
+      });
+    });
+  }); // end unloadWorkers
+
+  describe('publishHealthCheck', function() {
+    beforeEach(function(done) {
+      rabbitClient.hermesClient = {
+        publish: sinon.stub()
+      };
+      done();
+    });
+
+    it('should publish health-check', function(done) {
+      rabbitClient.hermesClient.publish.returns();
+
+      rabbitClient.publishHealthCheck();
+
+      expect(rabbitClient.hermesClient.publish
+        .withArgs('health-check').called).to.be.true();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].timestamp).to.exist();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].healthCheckId).to.exist();
+      done();
+    });
+  }); // end publishHealthCheck
+
+  describe('publishDockerHealthCheck', function() {
+    beforeEach(function(done) {
+      rabbitClient.hermesClient = {
+        publish: sinon.stub()
+      };
+      done();
+    });
+
+    it('should publish docker-health-check', function(done) {
+      var testData = {
+        dockerHost: 'testHost',
+        githubId: 1253543
+      };
+      rabbitClient.hermesClient.publish.returns();
+
+      rabbitClient.publishDockerHealthCheck(testData);
+
+      expect(rabbitClient.hermesClient.publish
+        .withArgs('docker-health-check').called).to.be.true();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].timestamp).to.exist();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].dockerHealthCheckId).to.exist();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].dockerHost).to.equal(testData.dockerHost);
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].githubId).to.equal(testData.githubId);
+      done();
+    });
+
+    it('should throw if missing keys', function(done) {
+      var testData = {
+        dockerHost: 'testHost',
+        githubId: 1253543
+      };
+
+      Object.keys(testData).forEach(function (key) {
+        var test = clone(testData);
+        delete test[key];
+        expect(function () {
+          rabbitClient.hermesClient.publishDockerHealthCheck(test);
+        }).to.throw();
+      });
+      done();
+    });
+  }); // end publishDockerHealthCheck
+
+  describe('publishOnDockUnhealthy', function() {
+    beforeEach(function(done) {
+      rabbitClient.hermesClient = {
+        publish: sinon.stub()
+      };
+      done();
+    });
+
+    it('should publish on-dock-unhealthy', function(done) {
+      var testData = {
+        host: 'testHost',
+        githubId: 1253543
+      };
+      rabbitClient.hermesClient.publish.returns();
+
+      rabbitClient.publishOnDockUnhealthy(testData);
+
+      expect(rabbitClient.hermesClient.publish
+        .withArgs('on-dock-unhealthy').called).to.be.true();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].timestamp).to.exist();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].dockerHealthCheckId).to.exist();
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].host).to.equal(testData.host);
+      expect(rabbitClient.hermesClient.publish
+        .args[0][1].githubId).to.equal(testData.githubId);
+      done();
+    });
+
+    it('should throw if missing keys', function(done) {
+      var testData = {
+        host: 'testHost',
+        githubId: 1253543
+      };
+
+      Object.keys(testData).forEach(function (key) {
+        var test = clone(testData);
+        delete test[key];
+        expect(function () {
+          rabbitClient.hermesClient.publishOnDockUnhealthy(test);
+        }).to.throw();
+      });
+      done();
+    });
+  }); // end publishOnDockUnhealthy
+
+  describe('_dataCheck', function() {
+    it('should throw if missing keys', function(done) {
+      var testData = {
+        summon: 'aeon',
+      };
+      expect(function () {
+        rabbitClient.constructor._dataCheck(testData, ['summon', 'spell']);
+      }).to.throw();
+      done();
+    });
+
+    it('should return if keys present', function(done) {
+      var testData = {
+        summon: 'aeon',
+      };
+      rabbitClient.constructor._dataCheck(testData, ['summon']);
+      done();
+    });
+  }); // end _dataCheck
+});
---
+++ test/unit/workers/base-worker.js
@@ -0,0 +1,143 @@
+'use strict';
+
+require('loadenv')();
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var describe = lab.describe;
+var it = lab.it;
+var afterEach = lab.afterEach;
+var beforeEach = lab.beforeEach;
+var Code = require('code');
+var expect = Code.expect;
+
+var sinon = require('sinon');
+var error = require('error-cat');
+
+var BaseWorker = require('../../../lib/workers/base-worker.js');
+
+describe('base-worker.js unit test', function () {
+  var baseWorker;
+  beforeEach(function (done) {
+    baseWorker = new BaseWorker({
+      name: 'test-name'
+    });
+
+    done();
+  });
+
+  describe('constructor', function () {
+    it('should set name and logData', function (done) {
+      var testName = 'faze';
+
+      var testWorker = new BaseWorker({
+        name: testName
+      });
+
+      expect(testWorker.name).to.equal(testName);
+      expect(testWorker.logData).to.exist();
+
+      done();
+    });
+
+    it('should throw if missing name', function (done) {
+      expect(function () {
+        new BaseWorker();
+      }).to.throw();
+      done();
+    });
+  }); // end constructor
+
+  describe('worker', function () {
+    beforeEach(function (done) {
+      sinon.stub(baseWorker, 'handle');
+      sinon.stub(baseWorker, 'isDataValid');
+      sinon.stub(error, 'create').returns();
+      done();
+    });
+
+    afterEach(function(done) {
+      error.create.restore();
+      done();
+    });
+
+    it('should run handle', function (done) {
+      var testData = {
+        summon: 'Icen'
+      };
+      baseWorker.handle.yieldsAsync();
+      baseWorker.isDataValid.returns(true);
+
+      baseWorker.worker(testData, function (err) {
+        expect(err).to.not.exist();
+        expect(baseWorker.handle.withArgs(testData).called)
+          .to.be.true();
+
+        done();
+      });
+    });
+
+    it('should not return handle err', function (done) {
+      var testErr = 'vine whip';
+      baseWorker.handle.yieldsAsync(testErr);
+      baseWorker.isDataValid.returns(true);
+
+      baseWorker.worker({}, function (err) {
+        expect(err).to.not.exist();
+        done();
+      });
+    });
+
+    it('should error if data invalid', function (done) {
+      baseWorker.isDataValid.returns(false);
+
+      baseWorker.worker({}, function (err) {
+        expect(err).to.not.exist();
+        expect(error.create.called).to.be.true();
+        expect(baseWorker.handle.called).to.be.false();
+
+        done();
+      });
+    });
+
+    it('should error if handle throws', function (done) {
+      var testErr = 'force winds';
+      sinon.stub(baseWorker, 'handleDomainError').returns();
+      baseWorker.handle.throws(testErr);
+      baseWorker.isDataValid.returns(true);
+
+      baseWorker.worker({}, function (err) {
+        expect(err).to.not.exist();
+        expect(baseWorker.handleDomainError.args[0][0].name)
+          .to.equal(testErr);
+
+        done();
+      });
+    });
+  }); // end worker
+
+  describe('handle', function() {
+    it('should throw if not implemented', function (done) {
+      expect(function () {
+        baseWorker.handle();
+      }).to.throw();
+      done();
+    });
+  }); // end handle
+
+  describe('isDataValid', function() {
+    it('should throw if not implemented', function (done) {
+      expect(function () {
+        baseWorker.isDataValid();
+      }).to.throw();
+      done();
+    });
+  }); // end isDataValid
+
+  describe('handleDomainError', function() {
+    it('should log error', function (done) {
+      baseWorker.handleDomainError();
+      done();
+    });
+  }); // end handleDomainError
+});
