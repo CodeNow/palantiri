e9aff5642ee151378564bf55400ac92d89f0e229
Nathan219 <Nathan219@gmail.com>
2015-12-09 17:54:19 -0800

    Adding functional test

--- test/funtional/index.js
+++ test/funtional/index.js
@@ -103,3 +103,56 @@ describe('functional test', function () {
     });
   });
 });
+
+describe('Unhealthy Test', function () {
+  var dockerStub;
+  var app;
+  beforeEach(function(done) {
+    process.env.COLLECT_INTERVAL = 100000;
+    process.env.RSS_LIMIT = 2000;
+    dockerStub = {
+      start: sinon.stub().yieldsAsync(new Error('Error response from daemon: Untar error on re-exec cmd: fork/exec /proc/self/exe: cannot allocate memory')),
+      remove: sinon.stub().yieldsAsync(),
+      logs: sinon.stub(),
+      modem: {
+        demuxStream: function (input, stdout) {
+          stdout.write(JSON.stringify({ info: { VmRSS: '1234 kb' } }));
+        }
+      }
+    };
+    sinon.stub(Dockerode.prototype, 'createContainer')
+      .yieldsAsync(null, dockerStub);
+    sinon.stub(ErrorCat.prototype, 'createAndReport');
+    sinon.stub(Docker.prototype, 'pullImage')
+      .yieldsAsync(null);
+    sinon.stub(request.Request, 'request');
+    app = new App();
+    app.start(done);
+  });
+
+  afterEach(function(done) {
+    delete process.env.COLLECT_INTERVAL;
+    delete process.env.RSS_LIMIT;
+    request.Request.request.restore();
+    Dockerode.prototype.createContainer.restore();
+    Docker.prototype.pullImage.restore();
+    ErrorCat.prototype.createAndReport.restore();
+    app.stop(done);
+  });
+  it('should emit unhealthy if start fails with out of memory error', function (done) {
+    process.env.RSS_LIMIT = 1;
+    var testHost = 'http://localhost:4242';
+
+    request.Request.request.yieldsAsync(null, null, [{
+      host: testHost,
+      tags: 'build,run,1111'
+    }]);
+
+    rabbitClient.hermesClient.subscribe('on-dock-unhealthy', function (data, cb) {
+      cb();
+      expect(data.host).to.equal(testHost);
+      expect(data.githubId).to.equal(1111);
+      done();
+    });
+  });
+});
