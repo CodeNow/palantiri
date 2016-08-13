edb41ed13475b8aa4d40e03157cc4ed84516c296
Anton Podviaznikov <podviaznikov@gmail.com>
2016-08-12 18:59:09 -0700

    code cleanup

--- test/unit/app.js
+++ test/unit/app.js
@@ -1,21 +1,21 @@
 'use strict'
 require('loadenv')()
 
-var Promise = require('bluebird')
-var Lab = require('lab')
-var lab = exports.lab = Lab.script()
-var describe = lab.describe
-var it = lab.it
-var afterEach = lab.afterEach
-var beforeEach = lab.beforeEach
-var Code = require('code')
-var expect = Code.expect
+const Promise = require('bluebird')
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const describe = lab.describe
+const it = lab.it
+const afterEach = lab.afterEach
+const beforeEach = lab.beforeEach
+const Code = require('code')
+const expect = Code.expect
 
-var sinon = require('sinon')
+const sinon = require('sinon')
 require('sinon-as-promised')(Promise)
-var rabbitClient = require('../../lib/external/rabbitmq.js')
-var App = require('../../lib/app.js')
-var workerServer = require('../../lib/workers/server')
+const rabbitClient = require('../../lib/external/rabbitmq.js')
+const App = require('../../lib/app.js')
+const workerServer = require('../../lib/workers/server')
 
 describe('app.js unit test', function () {
   var app
@@ -47,7 +47,7 @@ describe('app.js unit test', function () {
     it('should setup service', function (done) {
       rabbitClient.publishHealthCheck.returns()
 
-      app.start(function (err) {
+      app.start().asCallback(function (err) {
         expect(err).to.not.exist()
         expect(rabbitClient.connect.called).to.be.true()
         expect(app.interval).to.exist()
@@ -60,10 +60,10 @@ describe('app.js unit test', function () {
     })
 
     it('should err if connect failed', function (done) {
-      var testErr = 'flop'
+      const testErr = 'flop'
       rabbitClient.connect.rejects(testErr)
 
-      app.start(function (err) {
+      app.start().asCallback(function (err) {
         expect(err).to.exist()
         expect(rabbitClient.connect.called).to.be.true()
         expect(rabbitClient.publishHealthCheck.called).to.be.false()
@@ -88,7 +88,7 @@ describe('app.js unit test', function () {
     })
 
     it('should setup service', function (done) {
-      app.stop(function (err) {
+      app.stop().asCallback(function (err) {
         expect(err).to.not.exist()
         expect(app.interval).to.not.exist()
         done()
@@ -96,10 +96,10 @@ describe('app.js unit test', function () {
     })
 
     it('should err if close failed', function (done) {
-      var testErr = new Error('flop')
+      const testErr = new Error('flop')
       rabbitClient.disconnect.rejects(testErr)
 
-      app.stop(function (err) {
+      app.stop().asCallback(function (err) {
         expect(err.message).to.equal(testErr.message)
         expect(rabbitClient.disconnect.called).to.be.true()
         expect(app.interval).to.not.exist()
